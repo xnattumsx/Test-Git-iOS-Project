a866c12d78c74bf0d06b446c6366100ed47fc131
nsellers <nsellers@newforma.com>
2015-05-05 15:47:52 -0400

    added a test class

---
+++ NJSTest.h
@@ -0,0 +1,13 @@
+//
+//  NJSTest.h
+//  Git Test
+//
+//  Created by Nathan Sellers on 5/5/15.
+//  Copyright (c) 2015 Nathan Sellers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NJSTest : NSObject
+
+@end
---
+++ NJSTest.m
@@ -0,0 +1,13 @@
+//
+//  NJSTest.m
+//  Git Test
+//
+//  Created by Nathan Sellers on 5/5/15.
+//  Copyright (c) 2015 Nathan Sellers. All rights reserved.
+//
+
+#import "NJSTest.h"
+
+@implementation NJSTest
+
+@end
